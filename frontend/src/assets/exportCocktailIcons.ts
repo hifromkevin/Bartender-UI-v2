5d16f976db8809e1f5adb77ee63dc812b3aea43b
hifromkevin <lam.kevin86@gmail.com>
2025-04-27 19:01:06 -0700

    Creates export section for images

---
+++ frontend/src/assets/exportCocktailIcons.ts
@@ -0,0 +1,19 @@
+import chocolatini from './cocktails/chocolatini.jpg';
+import ginAndTonic from './cocktails/gin-and-tonic.jpg';
+import moscowMule from './cocktails/moscow-mule.jpg';
+import oldFashioned from './cocktails/old-fashioned.jpg';
+import rumAndCoke from './cocktails/rum-and-coke.jpg';
+import shirleyTemple from './cocktails/shirley-temple.jpg';
+import vodkaCran from './cocktails/vodka-cranberry.jpg';
+import vodkaSoda from './cocktails/vodka-soda.jpg';
+
+export {
+  chocolatini,
+  ginAndTonic,
+  moscowMule,
+  oldFashioned,
+  rumAndCoke,
+  shirleyTemple,
+  vodkaCran,
+  vodkaSoda,
+};
---
+++ frontend/src/assets/exportIcons.ts
@@ -0,0 +1,5 @@
+import cocktail from './icons/cocktail.png';
+import ice from './icons/ice.png';
+import wedge from './icons/wedge.png';
+
+export { cocktail, ice, wedge };
