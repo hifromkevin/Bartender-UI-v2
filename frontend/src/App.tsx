5376962f6527fd9679cafd666bec84a9074b1568
hifromkevin <lam.kevin86@gmail.com>
2025-04-27 19:02:58 -0700

    Plays with UI of different components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,32 @@
+import { useState } from 'react';
 import Stations from './features/Stations/Stations';
+import SelectedStation from './features/SelectedStation/SelectedStation';
 import Cocktails from './features/Cocktails/Cocktails';
 
 import logo from './assets/bartender-buddy-logo.png';
 
 import styles from './App.module.scss';
 
-const App = () => (
-  <div className={styles.appContainer}>
-    <div className={styles.appHeader}>
-      <img src={logo} alt="Bartender Buddy Logo" className={styles.logo} />
-    </div>
-    <div className={styles.appBodyContainer}>
-      <div className={styles.stationsContainer}>
-        <Stations />
-        <div className={styles.verticalLine}> </div>
+const App = () => {
+  const [isStationSelected, setIsStationSelected] = useState<boolean>(false);
+
+  return (
+    <div className={styles.appContainer}>
+      <div className={styles.appHeader}>
+        <img src={logo} alt="Bartender Buddy Logo" className={styles.logo} />
       </div>
-      <div className={styles.cocktailsContainer}>
-        <Cocktails />
+
+      <div className={styles.appBodyContainer}>
+        <div className={styles.stationsContainer}>
+          <Stations setIsStationSelected={setIsStationSelected} />
+          <div className={isStationSelected ? '' : styles.verticalLine}> </div>
+        </div>
+        <div className={styles.cocktailsContainer}>
+          {isStationSelected ? <SelectedStation /> : <Cocktails />}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
