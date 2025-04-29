dfada94d232f6b09cc5216c2561fec88e7a8c30c
hifromkevin <lam.kevin86@gmail.com>
2025-04-28 20:51:13 -0700

    Adds color coding for difficulty of bartender task

--- frontend/src/features/Cocktails/Cocktails.tsx
+++ frontend/src/features/Cocktails/Cocktails.tsx
@@ -1,10 +1,15 @@
-import { chocolatini, moscowMule } from '../../assets/exportCocktailIcons';
+import {
+  chocolatini,
+  moscowMule,
+  ginAndTonic,
+} from '../../assets/exportCocktailIcons';
 
 import styles from './styles/Cocktails.module.scss';
 
 const cocktailImages: Record<string, string> = {
   'Moscow Mule': moscowMule,
   Chocolatini: chocolatini,
+  'Gin and Tonic': ginAndTonic,
 };
 
 const getCocktailImage = (cocktailName: string): string =>
@@ -20,6 +25,7 @@ const fakeCocktails = [
     ice_type: 'Cubed',
     requires_shaker: true,
     instructions_id: 1,
+    difficulty: 'Medium',
   },
   {
     id: 1,
@@ -30,6 +36,28 @@ const fakeCocktails = [
     ice_type: null,
     requires_shaker: false,
     instructions_id: 2,
+    difficulty: 'Hard',
+  },
+  {
+    id: 1,
+    name: 'Gin and Tonic',
+    description: 'A refreshing cocktail made with gin, tonic water, and lime.',
+    glass_type: 'Cocktail',
+    ice_type: 'Cubed',
+    requires_shaker: false,
+    instructions_id: 1,
+    difficulty: 'Easy',
+  },
+  {
+    id: 1,
+    name: 'Chocolatini',
+    description:
+      'A rich, luxurious cocktail made with vodka, chocolate liqueur, and cream.',
+    glass_type: 'Martini',
+    ice_type: null,
+    requires_shaker: false,
+    instructions_id: 2,
+    difficulty: 'Hard',
   },
   {
     id: 1,
@@ -40,6 +68,7 @@ const fakeCocktails = [
     ice_type: 'Cubed',
     requires_shaker: true,
     instructions_id: 1,
+    difficulty: 'Hard',
   },
   {
     id: 1,
@@ -50,28 +79,22 @@ const fakeCocktails = [
     ice_type: null,
     requires_shaker: false,
     instructions_id: 2,
-  },
-  {
-    id: 1,
-    name: 'Moscow Mule',
-    description:
-      'A classic cocktail made with whiskey, lemon juice, and sugar.',
-    glass_type: 'Cocktail',
-    ice_type: 'Cubed',
-    requires_shaker: true,
-    instructions_id: 1,
-  },
-  {
-    id: 1,
-    name: 'Chocolatini',
-    description:
-      'A rich, luxurious cocktail made with vodka, chocolate liqueur, and cream.',
-    glass_type: 'Martini',
-    ice_type: null,
-    requires_shaker: false,
-    instructions_id: 2,
+    difficulty: 'Hard',
   },
 ];
+
+const setDifficultyColor = (difficulty: string): string => {
+  switch (difficulty) {
+    case 'Easy':
+      return styles.easy;
+    case 'Medium':
+      return styles.medium;
+    case 'Hard':
+      return styles.hard;
+    default:
+      return '';
+  }
+};
 
 const Cocktails = () => {
   return (
@@ -85,7 +108,20 @@ const Cocktails = () => {
               alt={cocktail.name}
               className={styles.cocktailImage}
             />
-            <h4>{cocktail.name}</h4>
+            <h4 className={styles.cocktailName}>
+              <span
+                className={`${styles.difficulty} ${setDifficultyColor(cocktail.difficulty)}`}
+              ></span>
+              {cocktail.name}
+            </h4>
+            {/* <h4
+              className={`${styles.cocktailName} ${setDifficultyColor(cocktail.difficulty)}`}
+            >
+              <span
+                className={`${styles.difficulty} ${setDifficultyColor(cocktail.difficulty)}`}
+              ></span>
+              {cocktail.name}
+            </h4> */}
           </div>
         ))}
       </section>
