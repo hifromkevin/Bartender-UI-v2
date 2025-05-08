bdd861f2535f30fffb55b751bdfa1fa59a4e6df9
hifromkevin <lam.kevin86@gmail.com>
2025-05-07 22:36:53 -0700

    Adds Go Types

--- backend/types/cocktail_type.go
+++ backend/types/cocktail_type.go
@@ -8,8 +8,8 @@ type Cocktail struct {
 	Description   string 		`json:"description"`
 	IsAlcoholic   bool 		 	`json:"is_alcoholic"`
 	ImageURL      string 		`json:"image_url"`
-	LastUsedAt		string  	`json:"last_used_at"`
-}
+	LastUsedAt		time.Time  	`json:"last_used_at"`
+};
 
 type CocktailDetails struct {
 	Cocktail     *Cocktail             `json:"cocktail"`
@@ -17,19 +17,19 @@ type CocktailDetails struct {
 	Ingredients  []CocktailIngredient  `json:"ingredients"`
 	Tools        []CocktailTool        `json:"tools"`
 	Glass        *CocktailGlass        `json:"glass"`
-}
+};
 
 type CocktailStep struct {
-  Id						string			`json:"id"`
-  CocktailId		string			`json:"cocktail_id"`
+  ID						string			`json:"id"`
+  CocktailID		string			`json:"cocktail_id"`
   StepNumber		int					`json:"step_number"`
   Instruction		string			`json:"instruction"`
 };
 
 type CocktailIngredient struct {
-	Id							string			`json:"id"`
-  CocktailId			string			`json:"cocktailId"`
-	IngredientId		string		  `json:"ingredientId"`
+	ID							string			`json:"id"`
+  CocktailID  		string			`json:"cocktail_id"`
+	IngredientID  	string		  `json:"ingredient_id"`
   Amount		    	int					`json:"amount"`
 	UnitOfMeasure		string			`json:"unit_of_measure"`
   IsAutomatic			bool				`json:"is_automatic"`
@@ -40,18 +40,18 @@ type CocktailTool struct {
   Id						string			`json:"id"`
 	Name					string			`json:"name"`
 	Type					string			`json:"type"`
-	ImageUrl		  string			`json:"imageUrl"`
+	ImageUrl		  string			`json:"image_url"`
 };
 
 type CocktailGlass struct { 
   Id						string			`json:"id"`
 	Name					string			`json:"name"`
 	Type					string			`json:"type"`
-	ImageUrl		  string			`json:"imageUrl"`
+	ImageUrl		  string			`json:"image_url"`
 };
 
 type CocktailUsage struct {
 	ID         string    `json:"id"`
 	CocktailID string    `json:"cocktail_id"`
 	MadeAt     time.Time `json:"made_at"`
-}
+};
--- backend/types/station_type.go
+++ backend/types/station_type.go
@@ -2,6 +2,7 @@ package types
 
 type Station struct {
   Id				     string						 `json:"id"`
-  Position	     int							 `json:"position"`		
+  Position	     int							 `json:"position"`	
+  IngredientID	 string						 `json:"ingredient_id"`	
   Ingredient     *Ingredient       `json:"ingredient"`
 };
