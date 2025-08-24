72e2114656a5979e68178df97785f14a290daaaf
hifromkevin <lam.kevin86@gmail.com>
2025-08-23 21:57:37 -0700

    Adds QueryClient

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,24 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import App from './App.tsx';
 
+import './index.css';
 import './main.scss';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
