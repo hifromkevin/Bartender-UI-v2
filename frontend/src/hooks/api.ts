4c8ab9b610e68fc34355b3a5ef421b15bf51a518
hifromkevin <lam.kevin86@gmail.com>
2025-09-10 22:12:53 -0700

    Handles default Vite failed API response

--- frontend/src/hooks/api.ts
+++ frontend/src/hooks/api.ts
@@ -5,21 +5,30 @@ const getAxiosClient = () =>
     baseURL: process.env.REACT_API_BASE_URL,
   });
 
+const validateNonHtmlResponse = (res: any) => {
+  if (res.headers['content-type'].includes('text/html')) {
+    throw new Error('Unexpected response. Expected JSON.');
+  }
+
+  return res.data;
+};
+
 export const getRequestString = async (URL: string): Promise<string> => {
-  const { data } = await getAxiosClient().get<string>(URL, {
+  const res = await getAxiosClient().get<string>(URL, {
     responseType: 'text',
   });
-  return data;
+
+  return validateNonHtmlResponse(res);
 };
 
 export const getRequest = async <T>(URL: string): Promise<T> => {
-  const { data } = await getAxiosClient().get<T>(URL);
-  return data;
+  const res = await getAxiosClient().get<T>(URL);
+  return validateNonHtmlResponse(res);
 };
 
 export const postRequest = async <T>(URL: string, queryData: any) => {
-  const { data } = await getAxiosClient().post<T>(URL, queryData);
-  return data;
+  const res = await getAxiosClient().post<T>(URL, queryData);
+  return validateNonHtmlResponse(res);
 };
 
 export const updateRequest = async (
