7f50fc88b33ed5cda16425802cbb03764c7f76ad
hifromkevin <lam.kevin86@gmail.com>
2025-09-20 23:54:43 -0700

    Rearranging

--- backend/types/cocktail_type.go
+++ backend/types/cocktail_type.go
@@ -1,11 +1,47 @@
 package types
 
 type Cocktail struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	IsAlcoholic bool   `json:"is_alcoholic"`
-	ImageURL    string `json:"image_url"`
+	ID             string  `db:"id" json:"id"`
+	Name           string  `db:"name" json:"name"`
+	Description    *string `db:"description" json:"description"`
+	IsAlcoholic    bool    `db:"is_alcoholic" json:"is_alcoholic"`
+	ImageURL       string  `db:"image_url" json:"image_url"`
+	RequiresShaker bool    `db:"requires_shaker" json:"requires_shaker"`
+	GlassID        string  `db:"glass_id" json:"glass_id"`
+}
+
+type CocktailStep struct {
+	ID          string `db:"id" json:"id"`
+	CocktailID  string `db:"cocktail_id" json:"cocktail_id"`
+	StepNumber  int    `db:"step_number" json:"step_number"`
+	Instruction string `db:"instruction" json:"instruction"`
+}
+
+type CocktailIngredient struct {
+	ID               string  `db:"id" json:"id"`
+	CocktailID       string  `db:"cocktail_id" json:"cocktail_id"`
+	IngredientID     string  `db:"ingredient_id" json:"ingredient_id"`
+	Amount           float64 `db:"amount" json:"amount"`
+	UnitOfMeasure    string  `db:"unit_of_measure" json:"unit_of_measure"`
+	IsAutomatic      bool    `db:"is_automatic" json:"is_automatic"`
+	Category         string  `db:"category" json:"category"`                   // 'mixer' | 'manual_ingredient' | 'garnish';
+	HasAutoOption    bool    `db:"has_auto_option" json:"has_auto_option"`     // true if it can be auto OR manual, like "Lime Juice" which can be fresh or bottled
+	AlternativeGroup string  `db:"alternative_group" json:"alternative_group"` // same group ID means "or". EX: "limeWheel1234" and "pineChunk5678" both have alternative_group = "garnish1".
+}
+
+type CocktailTool struct {
+	CocktailID string `db:"cocktail_id" json:"cocktail_id"`
+	ToolID     string `db:"tool_id" json:"tool_id"`
+}
+
+type CocktailFlavor struct {
+	CocktailID string `db:"cocktail_id" json:"cocktail_id"`
+	FlavorID   string `db:"flavor_id" json:"flavor_id"`
+}
+
+type CocktailFlavorDescriptor struct {
+	CocktailID         string `db:"cocktail_id" json:"cocktail_id"`
+	FlavorDescriptorID string `db:"flavor_descriptor_id" json:"flavor_descriptor_id"`
 }
 
 type CocktailDetails struct {
@@ -13,29 +49,4 @@ type CocktailDetails struct {
 	Steps       []CocktailStep       `json:"steps"`
 	Ingredients []CocktailIngredient `json:"ingredients"`
 	Tools       []CocktailTool       `json:"tools"`
-}
-
-type CocktailStep struct {
-	ID          string `json:"id"`
-	CocktailID  string `json:"cocktail_id"`
-	StepNumber  int    `json:"step_number"`
-	Instruction string `json:"instruction"`
-}
-
-type CocktailIngredient struct {
-	ID               string `json:"id"`
-	CocktailID       string `json:"cocktail_id"`
-	IngredientID     string `json:"ingredient_id"`
-	Amount           int    `json:"amount"`
-	UnitOfMeasure    string `json:"unit_of_measure"`
-	IsAutomatic      bool   `json:"is_automatic"`
-	Category         string `json:"category"`          // 'mixer' | 'manual_ingredient' | 'garnish';
-	HasAutoOption    bool   `json:"has_auto_option"`   // true if it can be auto OR manual, like "Lime Juice" which can be fresh or bottled
-	AlternativeGroup string `json:"alternative_group"` // same group ID means "or". EX: "limeWheel1234" and "pineChunk5678" both have alternative_group = "garnish1".
-}
-
-type CocktailTool struct {
-	Id         string `json:"id"`
-	CocktailID string `json:"cocktail_id"`
-	ToolID     string `json:"tool_id"`
 }
